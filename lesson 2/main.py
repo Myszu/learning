8b6d524b601edb049f5ab273f865b90a5cc9969e
Marcin <nowacki0508@gmail.com>
2026-03-23 20:45:30 +0100

    Added lesson 2

---
+++ lesson 2/main.py
@@ -0,0 +1,20 @@
+liczby = [1,2,3,4,5,6,7,8,9]
+parzyste = []
+nieparzyste = []
+
+# for liczba in liczby:
+#     if liczba % 2 == 0:
+#         parzyste.append(liczba)
+#     else:
+#         nieparzyste.append(liczba)
+        
+# parzyste = [liczba for liczba in liczby if liczba % 2 == 0]
+
+# liczby2 = [parzyste.append(liczba) if liczba % 2 == 0 else nieparzyste.append(liczba) for liczba in liczby]
+        
+with open('test.txt', mode="a", encoding='utf8') as file:
+    file.write('\nTest polskich znaków ĄĆĘŻÓŁ ')
+    
+file = open('test.txt', mode="w", encoding='utf8')
+file.write('\nTest numer 2')
+file.close()
